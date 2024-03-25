a65881a72ff3b2d38886d8858e915b910823ae14
haithamrdina <hrdina@ideolearning.com>
2024-03-25 13:59:07 +0000

    update config

--- app/app/Http/Integrations/Novi/Requests/GetMemberDetailFromNovi.php
+++ app/app/Http/Integrations/Novi/Requests/GetMemberDetailFromNovi.php
@@ -64,17 +64,17 @@ class GetMemberDetailFromNovi extends Request
 
             // Modify the value of the "Gender" key
             if ($userFields[$key] === 'Gender') {
-                // Check if the value is 'Male' or 'Prefer Not to Answer' and assign 9 or 11 accordingly
                 $value = ($value === 'Male') ? 9 : (($value === 'Prefer Not to Answer') ? 11 : null);
             }
 
             if ($userFields[$key] === 'CustomerType') {
-                // Check if the value is 'Male' or 'Prefer Not to Answer' and assign 9 or 11 accordingly
                 $value = ($value === 'Person') ? 7 : 8;
             }
             if ($userFields[$key] === 'MemberStatus') {
-                // Check if the value is 'Male' or 'Prefer Not to Answer' and assign 9 or 11 accordingly
                 $value = ($value === 'non_member') ? 9 : null;
+            }
+            if ($userFields[$key] === 'OpenDuesBalance') {
+                $value = strval($value);
             }
         }
         unset($value);
