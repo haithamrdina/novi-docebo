1516ae1857af01222f32b0dabca245c663141540
haithamrdina <hrdina@ideolearning.com>
2024-03-28 11:30:04 +0000

    check webhook status

--- app/routes/web.php
+++ app/routes/web.php
@@ -66,7 +66,7 @@ Route::get('check-docebo-user', function(){
 
 /** webhooks @s */
 Route::post('novi-listener', [WebHookController::class, 'webhookNoviHandler']);
-Route::post('novi-update-listener', [WebHookController::class, 'noviUpdateHandle']);
+Route::post('novi-update-listener', [WebHookController::class, 'webhookNoviUpdateHandler']);
 Route::post('docebo-listener', [WebHookController::class, 'webhookDoceboHandler']);
 Route::post('novi-updated', [WebHookController::class, 'noviUpdateHandle']);
 
