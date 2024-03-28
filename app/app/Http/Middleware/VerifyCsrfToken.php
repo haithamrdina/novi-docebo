cfda2a3869640f39994ace71694d03dda919a5d7
haithamrdina <hrdina@ideolearning.com>
2024-03-28 12:11:50 +0000

    test

--- app/app/Http/Middleware/VerifyCsrfToken.php
+++ app/app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/novi-listener',
-        '/docebo-listener'
+        '/docebo-listener',
+        '/novi-update-listener'
     ];
 }
