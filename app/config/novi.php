e84409f97e078b052c4669a94c88914da57137dd
haithamrdina <hrdina@ideolearning.com>
2024-03-29 00:10:57 +0000

    fix bug

--- app/config/novi.php
+++ app/config/novi.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    'endpoint' => "https://www.aaid.com",
-    'apikey' => "5GnzUogr6ometNrwsIgRDcUuQbpUvVeCnLpu8qmd2a8=",
+    'endpoint' => "https://aaid.novistaging.com",
+    'apikey' => "Y0KMAQrBBo5sAKu72GkddpzTzWLJz3aXWnwyEWQ+uP8=",
 ];
