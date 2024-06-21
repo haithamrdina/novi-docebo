a1f6fbd6795ab376e3c60cf8c96db45af4202177
haithamrdina <hrdina@ideolearning.com>
2024-06-21 18:09:08 +0100

    add agd number to creation

--- app/app/Http/Integrations/Docebo/Requests/GetUsersData.php
+++ app/app/Http/Integrations/Docebo/Requests/GetUsersData.php
@@ -16,7 +16,8 @@ class GetUsersData extends Request implements Paginatable
 
     public function __construct(
         protected string $email,
-    ) { }
+    ) {
+    }
     /**
      * The endpoint for the request
      */
@@ -36,20 +37,27 @@ class GetUsersData extends Request implements Paginatable
     public function createDtoFromResponse(Response $response): mixed
     {
         $item = $response->json('data.items');
-        if(!empty($item)){
+        $key = array_search('AGD Number', config('userfields'));
+        if (!empty($item)) {
+            $agdNumber = $item[0]['field_' . $key];
             $data = [
                 'Name' => $item[0]['fullname'],
                 "FirstName" => $item[0]['first_name'],
                 "LastName" => $item[0]['last_name'],
                 "Active" => true,
                 "Email" => $item[0]['email'],
-                "AccountEmail" =>  $item[0]['email'],
+                "AccountEmail" => $item[0]['email'],
                 "Approved" => true,
                 "CustomerType" => "Person",
                 "CustomFields" => [
                     "User created by" => [
                         "CustomerUniqueID" => "7a013cd3-a30a-4507-a887-5da26527f285",
                         "Value" => "docebo",
+                        "IsSumOfChildren" => false
+                    ],
+                    "AGD Number" => [
+                        "CustomerUniqueID" => "c0857a52-342b-47f9-815a-f6efe4d90edc",
+                        "Value" => $agdNumber,
                         "IsSumOfChildren" => false
                     ]
                 ]
